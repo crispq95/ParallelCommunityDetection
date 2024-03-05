0f130892e19b3289f2fb2d71c9349f9070093905
Cristina <you@example.com>
2024-03-05 19:04:18 +0100

    Added graph test file

--- GraphStructure/graph_test.c
+++ GraphStructure/graph_test.c
@@ -142,12 +142,12 @@ int main(int argc, char** argv) {
     std::vector<bool> is_boundary;
 
     // std::string filename = "/home/crispq/label_propagation/ParallelCommunityDetection/GraphExamples/0_karate_club_metis.txt";
-    std::string filename = "/home/crispq/label_propagation/ParallelCommunityDetection/GraphExamples/small_test.txt";
+    // std::string filename = "/home/crispq/parallelCommunityDetection/GraphExamples/small_test.txt";
+    std::string filename = "/home/crispq/parallelCommunityDetection/GraphExamples/0_karate_club_metis.txt";
 
     /* Load graph from file & Construct graph into CSR format */
     // Each process loads its portion of the vertices of the graph 
-    load_graph(filename, rank, world_size, &vtx_dist, &xadj, &adjncy, &global_ids, &is_boundary);
-
+    // load_graph(filename, rank, world_size, &vtx_dist, &xadj, &adjncy, &global_ids, &is_boundary);
     
     // if(rank==0)
     //     std::cout << "1st load done" << std::endl;
@@ -229,7 +229,9 @@ int main(int argc, char** argv) {
     // if(rank==0){
     //     std::cout << "Loading a graph..." << std::endl; 
     //     }
+    // std::cout << "Loading the graph" << std::endl;
     graph.create_graph_from_METIS(filename);
+    // std::cout << "LoadED !!" << std::endl;
 
     cm.init_communications(graph.ghost_nodes); 
 
@@ -249,126 +251,199 @@ int main(int argc, char** argv) {
 
     /* TO DO - RUN LPA */
     /* DEBUG __ Checking local labels */
-    // if(rank == 2){
-    //     std::cout << "Ghost on rank " << rank << " has id " << 2 << " and global id is " << graph.ghost_global_ids[2] << " local id of the ghost : " << (*graph.ghost_nodes)[graph.from_ghost_global_to_index(3)].id << std::endl; 
-    //     std::cout << "Ghost on rank " << rank << " has id " << 5 << " and global id is " << graph.ghost_global_ids[5] << " local id of the ghost : " << (*graph.ghost_nodes)[graph.from_ghost_global_to_index(5)].id << std::endl; 
+    // int check_r = 2;
+    // if(rank == check_r){
+    //     // std::cout << "Ghost on rank " << rank << " has id " << 2 << " and global id is " << graph.ghost_global_ids[2] << " local id of the ghost : " << (*graph.ghost_nodes)[graph.from_ghost_global_to_index(3)].id << std::endl; 
+    //     // std::cout << "Ghost on rank " << rank << " has id " << 5 << " and global id is " << graph.ghost_global_ids[5] << " local id of the ghost : " << (*graph.ghost_nodes)[graph.from_ghost_global_to_index(5)].id << std::endl; 
     //     // std::cout << "Ghost on rank " << rank << " has id " << 8 << " and global id is " << graph.from_ghost_global_to_index(8) << " local id of the ghost : " << (*graph.ghost_nodes)[graph.from_ghost_global_to_index(8)].id << std::endl; 
 
 
     //     // int cnt_b = 0;
 
+    //     std::cout << "BEFORE Local vtx on " << rank << " [id:label]: "; 
+    //     for(auto local_vtx : (*graph.local_nodes)){
+    //         // if(local_vtx.is_boundary){
+    //         //     std::cout << "vtx w local id : " << local_vtx.id << " [global:" << graph.from_local_to_global(local_vtx.id) << "]" << " is boundary" << std::endl;
+    //         //     cnt_b++;
+    //         // }
+    //         std::cout << local_vtx.id << " - " << local_vtx.current_label << std::endl; 
+    //     }
 
-    //     // for(auto local_vtx : (*graph.ghost_nodes)){
-    //     //     // if(local_vtx.is_boundary){
-    //     //     //     std::cout << "vtx w local id : " << local_vtx.id << " [global:" << graph.from_local_to_global(local_vtx.id) << "]" << " is boundary" << std::endl;
-    //     //     //     cnt_b++;
-    //     //     // }
-    //     //     std::cout << local_vtx.id << " - " << local_vtx.current_label << std::endl; 
-    //     // }
+    //     std::cout << "BEFORE Ghost vtx on " << rank << " [id:label]: "; 
+    //     for(auto local_vtx : (*graph.ghost_nodes)){
+    //         // if(local_vtx.is_boundary){
+    //         //     std::cout << "vtx w local id : " << local_vtx.id << " [global:" << graph.from_local_to_global(local_vtx.id) << "]" << " is boundary" << std::endl;
+    //         //     cnt_b++;
+    //         // }
+    //         std::cout << local_vtx.id << " - " << local_vtx.current_label << std::endl; 
+    //     }
     //     // std::cout << "nº boundary " << cnt_b << std::endl;
     // }
 
     // run this until convergence 
     // for all local vtx : 
-    for(auto local_vtx : (*graph.local_nodes))
-    {
-        // init label counter & maximal labels 
-        std::unordered_map<T,T> label_cnt;
-        std::vector<T> max_labels; 
-        LABEL_T max_label_value = 0;
-
-        if(rank == 1)
-            std::cout << "I'm vtx : " << local_vtx.id << " with neighbors : " << std::endl;
-
-        // for all neighbors of the local vtx 
-        for(auto neigh : (*local_vtx.edges)){
-            T n_idx = neigh.target; 
-            LABEL_T n_label = -1; 
-            
-            if(rank == 1)
-                std::cout << n_idx  << " " << std::endl;
+    int nsteps = 0; 
+        while(nsteps < 20){
+        for(auto local_vtx : (*graph.local_nodes)){
+            // init label counter & maximal labels 
+            std::unordered_map<T,T> label_cnt;
+            std::vector<T> max_labels; 
+            LABEL_T max_label_value = 0;
 
             // if(rank == 1)
-            //     std::cout << "nº local vtx : " << graph.get_local_vtx() << std::endl;
-            // if its local vtx : 
-            // if(graph.get_vtx_begin() <= n_idx && n_idx < graph.get_vtx_end()){
-            if(n_idx < graph.get_local_vtx()){
-                n_label = (*graph.local_nodes)[n_idx].current_label; 
+            //     if(local_vtx.id == 1) // vtx 8 
+            //         std::cout << "I'm vtx 4 at step " << nsteps << " printing the label of my neighbors "; 
+
+            // for all neighbors of the local vtx 
+            for(auto neigh : (*local_vtx.edges)){
+                T n_idx = neigh.target; 
+                LABEL_T n_label = -1; 
                 
-                // if(rank == 1)
-                //     std::cout << "LOCAL neighbor " << n_idx << " has label " << n_label << std::endl;
-            }else{
-                int local_idx = graph.get_vtx_end()-n_idx;
-                local_idx = graph.from_local_ghost_to_index(n_idx); 
-                n_label = (*graph.ghost_nodes)[local_idx].current_label; 
+                if(n_idx < graph.get_local_vtx()){
+                    n_label = (*graph.local_nodes)[n_idx].current_label; 
+                }else{
+                    int local_idx =  graph.from_local_ghost_to_index(n_idx);
+                    n_label = (*graph.ghost_nodes)[local_idx].current_label; 
+                }
 
-                int original = -1 ; 
-                for(auto a : graph.ghost_global_ids)
-                    if (a.second == local_idx )
-                        original = a.first; 
-                // n_label = (*graph.ghost_nodes)[local_idx].current_label; 
-                // if(rank == 1)
-                //     std::cout << "My neighbor " << n_idx << " indexed " << local_idx << " has label " << n_label << " its ID beign originally " << original << std::endl;
+                if(rank == 1)
+                if(local_vtx.id == 1) 
+                        std::cout << n_label << " "; 
 
+                /* add value to label counter */
+                // if label_cnt does not have this label yet -> add it 
+                if(label_cnt.find(n_label) == label_cnt.end()){
+                    label_cnt[n_label] = 1;
+                }else{
+                    label_cnt[n_label] += 1; 
+                }
+
+                // if new label = maximal 
+                if(max_label_value < label_cnt[n_label]){
+                    max_label_value = label_cnt[n_label]; 
+                    max_labels.clear(); // resize to 0 ? 
+                    max_labels.push_back(n_label); // append new max label 
+
+                // if new label is the same value another one found previously
+                }else if (label_cnt[n_label] == max_label_value){
+                    max_labels.push_back(n_label);
+                }
             }
 
-            /* add value to label counter */
-            // if label_cnt does not have this label yet -> add it 
-            if(label_cnt.find(n_label) == label_cnt.end()){
-                label_cnt[n_label] = 1;
-            }else{
-                label_cnt[n_label] += 1; 
-            }
-
-            // if(rank == 1){
-            //     std::cout << "CURRENT label " << n_label << " has value : " << label_cnt[n_label] << " max now is :" << max_label_value << std::endl;
-            // }
-
-            // if new label = maximal 
-            if(max_label_value < label_cnt[n_label])
-            {
-
-                max_label_value = label_cnt[n_label]; 
-                max_labels.clear(); // resize to 0 ? 
-                max_labels.push_back(n_label); // append new max label 
+            // if there is at least one max label -> change label 
+            if(max_labels.size() >= 1){
                 // if(rank == 1)
-                //     std::cout << "IF:: LABEL " << n_label << " is now maximal " << max_labels.size() << std::endl;
+                //     if(local_vtx.id == 1) 
+                //     {  
+                //         std::cout << std::endl; 
+                //         std::cout << "Found " << max_labels.size() << " maximal labels for this vtx : ";
+                //         for( int i = 0; i < max_labels.size() ; i++){
+                //                 std::cout << max_labels[i] << " ";  
+                //         }
+                //         std::cout << std::endl;
+                //     }
 
-            // if new label is the same value another one found previously
-            }else if (label_cnt[n_label] == max_label_value){
-                max_labels.push_back(n_label);
-                // if(rank == 1)
-                //     std::cout << "ELSE:: Pushing into max_labels with size " << max_labels.size() << std::endl; 
-            }
-        }
+                int rng_label = std::rand() % max_labels.size();   // pick randomly one label
 
-        // if there is at least one max label -> change label 
-        if(max_labels.size() >= 1){
-            int rng_label = std::rand() % max_labels.size();   // pick randomly one label
+                if(max_labels[rng_label] != local_vtx.current_label){
+                    graph.set_next_label(max_labels[rng_label], local_vtx.id);  // assign new label to local vtx 
+                    // append changed vtx to queue to be sent ? 
+                    cm.addToSend(&graph, local_vtx); 
 
-            // if(rank == 1)
-            //     std::cout << "Keeping label nº " << rng_label << " -> " << max_labels[rng_label] << std::endl; 
-            
-            if(max_labels[rng_label] != local_vtx.current_label){
-                graph.set_next_label(max_labels[rng_label], local_vtx.id);  // assign new label to local vtx 
-                // append changed vtx to queue to be sent ? 
-                cm.addToSend(&graph, local_vtx); 
+                    // if(rank == 1)
+                    //     if(local_vtx.id == 1) 
+                    //         std::cout << "Adding next label as " << max_labels[rng_label];
+                }
             }
         }
-        if(rank == 1)
-            std::cout << "__ " << std::endl; 
-        // break;
+        // if(rank == 2)
+        //     std::cout << std::endl; 
+        cm.send_recv_data(&graph); 
+        graph.update_labels(); 
 
+        nsteps++; //number of LPA steps, change later 
     }
-    cm.send_recv_data(&graph); 
 
+    std::cout << "DONE WITH LPA" << std::endl; 
     // communications are to be done here -> send any modified boundary vtx, 
                                           // rcv any ghost sent by other PEs 
+    /* HOW TO ? */
+    // each PE = slice of the cake -> write in order in file 
+    std::string output_filename = "/home/crispq/parallelCommunityDetection/output_small_test.txt";
+    std::ofstream myOutputFile;
+
+    int current_writer = 0; 
+    bool done = false; 
+    while( !done ){
+        // std::cout << "On rank " << rank << "the current_writer is " << current_writer << std::endl;
+        if(rank == current_writer){
+            // open the file 
+            // std::cout << "Opening the file on " << rank << std::endl;
+            if(rank == 0)
+                myOutputFile.open(output_filename);
+            else
+                myOutputFile.open(output_filename, std::ios::app);
+
+            // std::cout << "Writting file on " << rank << std::endl;
+            // write into the output file its portion of the graph + labels 
+            for( auto local_vtx : (*graph.local_nodes) ){
+                T global_id = graph.from_local_to_global(local_vtx.id); 
+                myOutputFile << global_id << " " << local_vtx.current_label << std::endl; 
+            }
+
+            //close file 
+            // std::cout << "Closing the file on " << rank << std::endl;
+            myOutputFile.close(); 
+
+            // send the next writter a message 
+            current_writer++;
+            // std::cout << "Sending the msg from " << rank << " to " << rank+1 << " with current_writer " << current_writer << std::endl;
+            if(rank < world_size-1)
+                MPI_Send(&current_writer, 1, MPI_INT, rank+1, 30, MPI_COMM_WORLD);
+
+            // this one is done 
+            done = true;  
+        }else{
+            MPI_Recv(&current_writer, 1, MPI_INT, rank-1, 30, MPI_COMM_WORLD, MPI_STATUS_IGNORE);     // wait til getting message 
+        }
+    }
+
+    // std::cout << "Done with my writting from " << rank << std::endl;
 
 
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // calculate modularity ? 
+
+    // if(rank == check_r){
+    //     // std::cout << "Ghost on rank " << rank << " has id " << 2 << " and global id is " << graph.ghost_global_ids[2] << " local id of the ghost : " << (*graph.ghost_nodes)[graph.from_ghost_global_to_index(3)].id << std::endl; 
+    //     // std::cout << "Ghost on rank " << rank << " has id " << 5 << " and global id is " << graph.ghost_global_ids[5] << " local id of the ghost : " << (*graph.ghost_nodes)[graph.from_ghost_global_to_index(5)].id << std::endl; 
+    //     // std::cout << "Ghost on rank " << rank << " has id " << 8 << " and global id is " << graph.from_ghost_global_to_index(8) << " local id of the ghost : " << (*graph.ghost_nodes)[graph.from_ghost_global_to_index(8)].id << std::endl; 
+
+
+    //     // int cnt_b = 0;
+
+    //     std::cout << "AFTER Local vtx on " << rank << " [id:label]: "; 
+    //     for(auto local_vtx : (*graph.local_nodes)){
+    //         // if(local_vtx.is_boundary){
+    //         //     std::cout << "vtx w local id : " << local_vtx.id << " [global:" << graph.from_local_to_global(local_vtx.id) << "]" << " is boundary" << std::endl;
+    //         //     cnt_b++;
+    //         // }
+    //         std::cout << local_vtx.id << " - " << local_vtx.current_label << std::endl; 
+    //     }
+
+    //     std::cout << "AFTER Ghost vtx on " << rank << " [id:label]: "; 
+    //     for(auto local_vtx : (*graph.ghost_nodes)){
+    //         // if(local_vtx.is_boundary){
+    //         //     std::cout << "vtx w local id : " << local_vtx.id << " [global:" << graph.from_local_to_global(local_vtx.id) << "]" << " is boundary" << std::endl;
+    //         //     cnt_b++;
+    //         // }
+    //         std::cout << local_vtx.id << " - " << local_vtx.current_label << std::endl; 
+    //     }
+    //     // std::cout << "nº boundary " << cnt_b << std::endl;
+    // }
 
     MPI_Barrier(MPI_COMM_WORLD); 
-
     MPI_Finalize();
 }
 
